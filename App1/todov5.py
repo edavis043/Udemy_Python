4c61f6ab08ba9b2a365d56fb1dc047194ff408d1
edavis043 <edavis@ecloudcontrol.com>
2023-09-12 14:34:04 +0530

    Improved GUI.

--- App1/todov5.py
+++ App1/todov5.py
@@ -1,6 +1,9 @@
 import modules.todov4func1 as fn
 import PySimpleGUI as tk
+import time
 
+tk.theme("DarkTeal12")
+clock = tk.Text('', key='clock')
 label = tk.Text("Enter the Todo:")
 input_label = tk.InputText(tooltip="Enter the Todo Item.", key="todo")
 add_button = tk.Button("Add")
@@ -9,16 +12,17 @@ list_box = tk.Listbox(values=fn.get_todos(), key='todos',
 edit_button = tk.Button("Edit")
 complete_button = tk.Button("Complete")
 exit_button = tk.Button("Exit")
-window = tk.Window('ToDo v5.0', layout=[[label],
-                                        [input_label, add_button],
-                                        [list_box, edit_button, complete_button],
-                                        [exit_button]])
+window = tk.Window('ToDo v5.0',
+                   layout=[[clock],
+                           [label],
+                           [input_label, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
+                   font=("Helvetica", 14))
 
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
-    print(values['todos'])
+    event, values = window.read(timeout=1000)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M"))
     match event:
         case "Add":
             todos = fn.get_todos()
@@ -27,19 +31,26 @@ while True:
             fn.write_todos(todos)
             window['todos'].update(values=todos)
         case "Edit":
-            todo_to_edit = values['todos']
-            new_todo = values['todo']
-            todos = fn.get_todos()
-            index = todos.index()
-            todos[index] = new_todo
-            window['todos'].update(values=todos)
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
+                todos = fn.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                fn.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                tk.popup("Please select an Item!", font=("Helvetica", 14))
         case "Complete":
-            todo_to_remove = values['todos'][0]
-            todos = fn.get_todos()
-            todos.remove(todo_to_remove)
-            fn.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(values='')
+            try:
+                todo_to_remove = values['todos'][0]
+                todos = fn.get_todos()
+                todos.remove(todo_to_remove)
+                fn.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(values='')
+            except IndexError:
+                tk.popup("Please select an Item!", font=("Helvetica", 14))
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case 'Exit':
