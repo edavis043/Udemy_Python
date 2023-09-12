88432c9838313f4b745411f6f59120ace6ac2321
edavis043 <edavis@ecloudcontrol.com>
2023-09-12 11:07:47 +0530

    Implemented Edit Button.

--- App1/todov5.py
+++ App1/todov5.py
@@ -4,19 +4,33 @@ import PySimpleGUI as tk
 label = tk.Text("Enter the Todo:")
 input_label = tk.InputText(tooltip="Enter the Todo Item.", key="todo")
 add_button = tk.Button("Add")
+list_box = tk.Listbox(values=fn.get_todos(), key='todos', enable_events=True, size=[45, 10])
+edit_button = tk.Button("Edit")
 
-window = tk.Window('ToDo v5.0', layout=[[label], [input_label, add_button]])
+window = tk.Window('ToDo v5.0', layout=[[label], [input_label, add_button], [list_box, edit_button]] )
 
 while True:
     event, values = window.read()
     print(event)
     print(values)
+    print(values['todos'])
     match event:
         case "add":
             todos = fn.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             fn.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "edit":
+            todo_to_edit = values['todos']
+            new_todo = values['todo']
+            todos = fn.get_todos()
+            index = todos.index()
+            todos[index] = new_todo
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+
         case tk.WIN_CLOSED:
             break
 
