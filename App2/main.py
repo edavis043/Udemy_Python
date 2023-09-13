c885b8acc2ca1c02ce61637683e6cab9a6fa3458
edavis043 <edavis@ecloudcontrol.com>
2023-09-13 14:40:40 +0530

    Added Page contents.

--- App2/main.py
+++ App2/main.py
@@ -1,13 +1,30 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
-
 with col1:
     st.image("images/photo.png")
-
 with col2:
     st.title("Evin Davis")
     content = "This is a repo for projects that i have done during Python Udemy Course."
     st.info(content)
+
+content2 = "Here are the projects"
+st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+df = pd.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
