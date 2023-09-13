9ef19fe6c10e56d629a7091341f5c22b18714ba4
edavis043 <edavis@ecloudcontrol.com>
2023-09-13 17:26:15 +0530

    Create PDF file.

--- App3/main.py
+++ App3/main.py
@@ -2,11 +2,20 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv("topics.csv")
 
-for index,row in df.iterrows():
+for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font(family="Times", size=12)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 21, 200, 21)
+
+    pdf.ln(260)
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
 pdf.output("test.pdf")
